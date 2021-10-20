6bf35e6e61a7568db8c5832384d8e1e668c19d8e
DominikWawak <dominikwawak@gmail.com>
2021-10-20 21:57:18 +0100

    added upcoming movies page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import FavoriteMoviesPage from "./pages/favouriteMoviesPage.js";
+import upcomingMoviesPage from "./pages/upcomingMoviesPage.js";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
           <SiteHeader />      {/* New Header  */}
      
       <Switch>
+        <Route exact path="/movies/upcoming" component={upcomingMoviesPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
